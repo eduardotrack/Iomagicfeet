1b0e74346f7c8af64d6c1c60a9f4fc4c0e54cbd7
eduardotrack <eduardo.magno@trackdev.com.br>
2023-05-04 17:41:56 -0300

    Magic lp save1

---
+++ magicApp/react/save1.jsx
@@ -0,0 +1,157 @@
+import React, {useEffect} from 'react';
+
+const save1 = () => {
+    useEffect(() => {
+        const fonts = document.createElement('link');
+        fonts.rel = "stylesheet"
+        fonts.href = "https://fonts.googleapis.com/css2?family=Lexend:wght@100;200;300;400;500;600;700;800;900&amp;display=swap";
+
+        document.body.appendChild(fonts);
+
+        const styles = document.createElement('link');
+        styles.rel = "stylesheet"
+        styles.type = "text/css"
+        styles.href = "https://magicfeet.vtexassets.com/arquivos/save1-style.css?v=1";
+
+        document.body.appendChild(styles);
+
+        const styles1 = document.createElement('link');
+        styles1.rel = "stylesheet"
+        styles1.type = "text/css"
+        styles1.href = "https://magicfeet.vtexassets.com/arquivos/save1-font.css?v=1";
+
+        document.body.appendChild(styles1);
+
+        const styles2 = document.createElement('link');
+        styles2.rel = "stylesheet"
+        styles2.type = "text/css"
+        styles2.href = "https://magicfeet.vtexassets.com/arquivos/save1-home.css?v=2";
+
+        document.body.appendChild(styles2);
+      }, []);
+
+    return ( 
+        <>
+            <div class="save-1">
+                <div class="home-container">
+                    <div class="navbar-navbar-container">
+                        <header data-role="Header" class="navigation-container">
+                            <img alt="image" src="/arquivos/ativo-5.svg" class="navbar-image" />
+                            <div class="navbar-nav">
+                                <nav class="navigation-links-nav navigation-links-root-class-name10">
+                                    <span class="navigation-links-text navbar-link">
+                                        <span>
+                                            <a href="#a-ace1-agora-e-save1">
+                                            A ACE1 AGORA É SAVE1?
+                                            </a>
+                                        </span>
+                                    </span>
+
+                                    <span class="navigation-links-text1 navbar-link">
+                                        <span>QUERO CONHECER MAIS</span>
+                                    </span>
+                                </nav>
+                            </div>
+
+                            <a href="https://ace1br.app.link/save1afeetlpdownload" target="_blank"> 
+                                <button class="navbar-primary-btn1 button-primary button">
+                                    <span>Baixar o App</span>
+                                </button>
+                            </a>
+                        </header>
+                    </div>
+
+                    <main class="home-main">
+                        <div class="section-container">
+                            <div class="home-max-width max-content-container">
+                                <div class="home-content-container">
+                                    <h1 class="home-text">
+                                        <span class="home-text01">CONSUMO</span>
+                                        <span class="home-text02">SUSTENTÁVEL</span>
+                                        <br class="home-text03" />
+                                        <span class="home-text04">EM SUAS_MÃOS</span>
+                                    </h1>
+                                    <div class="home-input-container">
+                                        <a href="https://ace1br.app.link/save1afeetlpdownload" target="_blank"> <button class="home-primary-btn button-primary button">
+                            Baixar o App
+                        </button></a>
+                                        <a href="https://www.save1.com.br" target="_blank"><button class="home-primary-btn1 button-primary button">
+                            Quero Conhecer Mais
+                        </button></a>
+                                    </div>
+                                    <div class="home-features-container">
+                                        <div class="home-feature">
+                                            <svg viewBox="0 0 877.7142857142857 1024" class="home-icon">
+                            <path
+                                d="M733.714 419.429c0-9.714-3.429-19.429-10.286-26.286l-52-51.429c-6.857-6.857-16-10.857-25.714-10.857s-18.857 4-25.714 10.857l-233.143 232.571-129.143-129.143c-6.857-6.857-16-10.857-25.714-10.857s-18.857 4-25.714 10.857l-52 51.429c-6.857 6.857-10.286 16.571-10.286 26.286s3.429 18.857 10.286 25.714l206.857 206.857c6.857 6.857 16.571 10.857 25.714 10.857 9.714 0 19.429-4 26.286-10.857l310.286-310.286c6.857-6.857 10.286-16 10.286-25.714zM877.714 512c0 242.286-196.571 438.857-438.857 438.857s-438.857-196.571-438.857-438.857 196.571-438.857 438.857-438.857 438.857 196.571 438.857 438.857z"
+                            ></path>
+                            </svg>
+                                            <span class="home-text05">
+                            Compre produtos quase novos, 100% originais, por preços
+                            acessíveis
+                            </span>
+                                        </div>
+                                        <div class="home-feature1">
+                                            <svg viewBox="0 0 877.7142857142857 1024" class="home-icon2">
+                            <path
+                                d="M733.714 419.429c0-9.714-3.429-19.429-10.286-26.286l-52-51.429c-6.857-6.857-16-10.857-25.714-10.857s-18.857 4-25.714 10.857l-233.143 232.571-129.143-129.143c-6.857-6.857-16-10.857-25.714-10.857s-18.857 4-25.714 10.857l-52 51.429c-6.857 6.857-10.286 16.571-10.286 26.286s3.429 18.857 10.286 25.714l206.857 206.857c6.857 6.857 16.571 10.857 25.714 10.857 9.714 0 19.429-4 26.286-10.857l310.286-310.286c6.857-6.857 10.286-16 10.286-25.714zM877.714 512c0 242.286-196.571 438.857-438.857 438.857s-438.857-196.571-438.857-438.857 196.571-438.857 438.857-438.857 438.857 196.571 438.857 438.857z"
+                            ></path>
+                            </svg>
+                                            <span class="home-text06">
+                            Ganhe dinheiro vendendo os sneakers esquecidos em seu
+                            armário&nbsp;
+                            </span>
+                                        </div>
+                                        <div class="home-feature2">
+                                            <svg viewBox="0 0 877.7142857142857 1024" class="home-icon4">
+                            <path
+                                d="M733.714 419.429c0-9.714-3.429-19.429-10.286-26.286l-52-51.429c-6.857-6.857-16-10.857-25.714-10.857s-18.857 4-25.714 10.857l-233.143 232.571-129.143-129.143c-6.857-6.857-16-10.857-25.714-10.857s-18.857 4-25.714 10.857l-52 51.429c-6.857 6.857-10.286 16.571-10.286 26.286s3.429 18.857 10.286 25.714l206.857 206.857c6.857 6.857 16.571 10.857 25.714 10.857 9.714 0 19.429-4 26.286-10.857l310.286-310.286c6.857-6.857 10.286-16 10.286-25.714zM877.714 512c0 242.286-196.571 438.857-438.857 438.857s-438.857-196.571-438.857-438.857 196.571-438.857 438.857-438.857 438.857 196.571 438.857 438.857z"
+                            ></path>
+                            </svg>
+                                            <span class="home-text07">
+                            Venda e ganhe cashback para usar na Artwalk, Authentic
+                            Feet e Magic Feet
+                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="home-image-container">
+                                    <img alt="image" src="/arquivos/lp-grande-save1.jpg" class="home-image" />
+                                </div>
+                            </div>
+                        </div>
+                    </main>
+                    <div class="home-banner">
+                        <div class="home-testimonial" id="a-ace1-agora-e-save1">
+                            <img alt="image" src="/arquivos/lp-novaace1-500w.png" class="home-image1" />
+                            <div class="home-testimonial1">
+                                <span class="home-text08">
+                        <span>
+                        Dizem que a mudança é a única constante na vida. É por isso
+                        que estamos trocando nosso nome para SAVE1!
+                        </span>
+                                <br />
+                                <br />
+                                <span>
+                        Um nome que reflete nossos novos pilares estratégicos:
+                        Sustentabilidade, Autenticidade e Tecnologia.
+                        </span>
+                                <br />
+                                <br />
+                                <span>
+                        Nosso compromisso com a autenticidade nunca mudará. Na SAVE1,
+                        você pode comprar e vender sneakers usados em ótimas condições
+                        com a garantia de que são 100% originais, 100% das vezes.
+                        </span>
+                                <br />
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default save1;
