14cd57a6000baea3375c404fb3b59a8f6511a3f2
Pedro landim <pedrolandin@pandanjo.com>
2023-11-10 12:01:13 -0300

    Ajustes de texto de frete

--- magicApp/react/NeoassistCentral.jsx
+++ magicApp/react/NeoassistCentral.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React from 'react'
 
+import style from './NeoassistCentral.css'
 
-function NeoassistCentral(){ 
-    return(
-        <>
-            <div>
-                <div id="NeoassistCentral"></div>
-            </div>
-        </>
-    )
+function NeoassistCentral() {
+  return (
+    <>
+      <div className={`${style.bottomNeo}`}>
+        <div id="NeoassistCentral" />
+      </div>
+    </>
+  )
 }
 
-export default NeoassistCentral;
+export default NeoassistCentral
