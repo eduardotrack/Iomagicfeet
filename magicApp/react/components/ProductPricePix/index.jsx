51326c2d317465e33ae32f0f4c93f82008139f7e
bporato <brunaporato@pandanjo.com>
2025-06-23 16:07:12 -0300

    adiciona valor do pix no summary de categoria/search

--- magicApp/react/components/ProductPricePix/index.jsx
+++ magicApp/react/components/ProductPricePix/index.jsx
@@ -1,9 +1,7 @@
 import { useProduct } from "vtex.product-context"
 import styles from "./styles.css"
 
-const PIX_DISCOUNT_PERCENTAGE = 5
-
-export function ProductPricePix() {
+export function ProductPricePix({ isSummary = false, discountPercentage = 5 }) {
   const { product } = useProduct()
 
   const commercialOffer = product?.items?.[0]?.sellers?.[0]?.commertialOffer
@@ -16,7 +14,7 @@ export function ProductPricePix() {
 
   const isOffer = listPrice > sellingPrice
 
-  const pricePix = sellingPrice * (1 - PIX_DISCOUNT_PERCENTAGE / 100)
+  const pricePix = sellingPrice * (1 - discountPercentage / 100)
   const pricePixFormatted = pricePix.toFixed(2).replace(".", ",")
 
   let finalDiscountPercentage
@@ -26,18 +24,46 @@ export function ProductPricePix() {
     const preciseOfferDiscount = ((listPrice - sellingPrice) / listPrice) * 100
     const roundedOfferDiscount = Math.round(preciseOfferDiscount)
 
-    finalDiscountPercentage = roundedOfferDiscount + PIX_DISCOUNT_PERCENTAGE
+    finalDiscountPercentage = roundedOfferDiscount + discountPercentage
     oldPriceFormatted = listPrice.toFixed(2).replace(".", ",")
   } else {
-    finalDiscountPercentage = PIX_DISCOUNT_PERCENTAGE
+    finalDiscountPercentage = discountPercentage
     oldPriceFormatted = sellingPrice.toFixed(2).replace(".", ",")
+  }
+
+  if (isSummary) {
+    return (
+      <div className={styles.summaryPixContainer}>
+        <p className={styles.summaryPixPrice}>R$ {pricePixFormatted} no pix à vista</p>
+      </div>
+    )
   }
 
   return (
     <div className={styles.pixPriceContainer}>
       <p className={styles.pixPriceValue}>R$ {pricePixFormatted} no <span>pix à vista</span></p>
       <p className={styles.pixPriceOldValue}>R$ {oldPriceFormatted}</p>
-      <span className={styles.pixPriceDiscount}>{finalDiscountPercentage}% Off</span>
+      <span className={styles.pixPriceDiscount}>{finalDiscountPercentage}% OFF</span>
     </div>
   )
+}
+
+ProductPricePix.schema = {
+  title: "Panda | Preço com Pix",
+  description: "Configurações do componente de preço com desconto para pagamento via Pix.",
+  type: "object",
+  properties: {
+    isSummary: {
+      title: 'Resumo do produto (vitrine)',
+      type: 'boolean',
+      default: false,
+    },
+    discountPercentage: {
+      title: 'Desconto percentual para pagamento via Pix',
+      type: 'number',
+      default: 5,
+      minimum: 0,
+      maximum: 100,
+    }
+  }
 }
